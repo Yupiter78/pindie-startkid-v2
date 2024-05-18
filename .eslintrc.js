cefff46a80f7bfe38b251ba22de625e140f254c8
Yuriy Vedernikov <yu.piter.gm@gmail.com>
2024-05-18 23:19:05 +0300

    feat: configure server and client connection

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
         browser: true,
         es2021: true
     },
-    extends: ["standard", "plugin:react/recommended"],
+    extends: [
+        "standard",
+        "plugin:react/recommended",
+        "plugin:@next/eslint-plugin-next/recommended"
+    ],
     overrides: [
         {
             env: {
@@ -19,7 +23,12 @@ module.exports = {
         ecmaVersion: "latest",
         sourceType: "module"
     },
-    plugins: ["react"],
+    plugins: ["react", "@next/eslint-plugin-next"],
+    settings: {
+        react: {
+            version: "detect"
+        }
+    },
     rules: {
         semi: [2, "always"],
         indent: [0, 4],
@@ -38,6 +47,7 @@ module.exports = {
                 allowTemplateLiterals: true
             }
         ],
-        "multiline-ternary": [0]
+        "multiline-ternary": [0],
+        "no-new": 0
     }
 };
--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./globals.css";
 import { App } from "./App";
+import PropTypes from "prop-types";
 
 export const metadata = {
     title: "Pindie",
@@ -16,3 +17,7 @@ export default function RootLayout({ children }) {
         </html>
     );
 }
+
+RootLayout.propTypes = {
+    children: PropTypes.node.isRequired
+};
